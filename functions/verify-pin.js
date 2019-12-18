312753858c2ac4566d119cc5bc54ec3f5b8977f7
Joost Baaij <joost@spacebabies.nl>
2019-12-18 15:21:24 +0100

    added first stab at Faunadb integration

---
+++ functions/verify-pin.js
@@ -0,0 +1,12 @@
+import faunadb from 'faunadb'
+
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET
+})
+
+exports.handler = (event, context, callback) => {
+  const data = JSON.parse(event.body)
+
+  return client.query(q.Create(q.Ref()))
+}
